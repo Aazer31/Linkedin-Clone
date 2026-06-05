9bfb07026126fe9a04012844c0cd65c8455a0c54
Aazer <Ownersmith17@gmail.com>
2026-06-05 18:32:09 +0000

    feat: create User model with mongoose schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    coverImage: {
+      type: String,
+      default: "",
+    },
+    headline: {
+      type: String,
+      default: "",
+    },
+    skills:[ {type: String} ],
+    education: [
+      {
+        college: {type: string},
+        degree: {type: string},
+        fieldOfStudy: {type: string},
+      }
+      ],
+
+    location: {
+        type: string
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other"],
+    },
+    experience: [
+        {
+            title: {type: String},
+            company: {type: String},
+            description: {type: String},     
+        }
+     ],
+        connections: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+            }
+        ]
+  },{ timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
