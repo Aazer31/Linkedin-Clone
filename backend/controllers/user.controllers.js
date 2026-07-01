409f9e825ae47e28ac15680f8f7ec054a1536cd6
Aazer <Ownersmith17@gmail.com>
2026-07-01 22:25:31 +0000

    feat: setup image handling with Multer and Cloudinary

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js"
 export const getCurrentUser = async (req, res)=>{
     try{
         let id = req.userId
-        console.log(id)
+        // console.log(id)
         const user = await User.findById(id).select("-password")
         if(!user){
             return res.status(400).json({message: "user does not found"})
